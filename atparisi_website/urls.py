65f4567801a81a27e3aa75188a36f901f00a2f7a
Alex Parisi <alex.parisi@outlook.com>
2023-05-31 11:32:03 -0400

    Update urls.py

--- atparisi_website/urls.py
+++ atparisi_website/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from .views import home
+from django.views.static import serve
+import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("projects/", include("projects.urls")),
     path('', home, name='home'),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
