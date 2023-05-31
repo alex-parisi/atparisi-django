b863291d76faf0b551ba958d0cf83d0d56993d9b
Alex Parisi <alex.parisi@outlook.com>
2023-05-31 10:58:30 -0400

    Update settings.py

--- atparisi_website/settings.py
+++ atparisi_website/settings.py
@@ -122,6 +122,8 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
