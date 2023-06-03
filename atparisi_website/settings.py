5b595605f37b74b712f6166cfa51170e6aabca20
Alex Parisi <alex.parisi@outlook.com>
2023-06-02 23:21:08 -0400

    preparing for merge

--- atparisi_website/settings.py
+++ atparisi_website/settings.py
@@ -121,7 +121,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
